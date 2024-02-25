35dcd1954096be796cfadc4e94794c4307cc6797
tasano <13191864+tasano@users.noreply.github.com>
2024-02-25 16:36:15 +0900

    Season-2 Level-5

--- Season-2/Level-5/code.js
+++ Season-2/Level-5/code.js
@@ -15,6 +15,10 @@ var CryptoAPI = (function() {
 			size: 20,
 			block: 64,
 			hash: function(s) {
+				if (typeof s !== "string") {
+					throw "Error: CryptoAPI.sha1.hash() should be called with a 'normal' parameter (i.e., a string)";
+				}
+				
 				var len = (s += '\x80').length,
 					blocks = len >> 6,
 					chunk = len & 63,
@@ -22,9 +26,18 @@ var CryptoAPI = (function() {
 					i = 0,
 					j = 0,
 					H = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0],
-					w = [];
-					
-				while (chunk++ != 56) {
+					w = [
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+					];
+
+					while (chunk++ != 56) {
 					s += "\x00";
 					if (chunk == 64) {
 						blocks++;
@@ -42,7 +55,7 @@ var CryptoAPI = (function() {
 						w[(i >> 2) & 15] = j;
 						j = 0;
 					}
-					if ((i & 63) == 63) CryptoAPI.sha1._round(H, w);
+					if ((i & 63) == 63) internalRound(H, w);
 				}
 				
 				for (i = 0; i < H.length; i++)
@@ -53,6 +66,8 @@ var CryptoAPI = (function() {
 			_round: function(H, w) { }
 		} // End "sha1"
 	}; // End "API"
+
+	var internalRound = API.sha1._round;
 
 	return API; // End body of anonymous function
 })(); // End "CryptoAPI"
